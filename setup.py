d901f7df8d4029c83b99d46e6ffde33cec3ed54c
clach04 <clach04@gmail.com>
2024-01-27 09:04:03 -0800

    setup.py rename to lowercase, bump version to make it different

--- setup.py
+++ setup.py
@@ -1,11 +1,36 @@
-from distutils.core import setup
+import sys
 
-setup(name='Stache',
-      version='0.0.9',
-      description='Trimmed mustache logic-less templates',
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if len(sys.argv) <= 1:
+    print("""
+Suggested setup.py parameters:
+
+    * build
+    * install
+    * sdist  --formats=zip
+    * sdist  # NOTE requires tar/gzip commands
+
+    python -m pip install -e .
+
+PyPi:
+
+    python -m pip install setuptools twine
+    twine upload dist/*
+    ./setup.py  sdist ; twine upload dist/* --verbose
+
+""")
+
+setup(name='stache',  # TODO rename
+      version='0.0.10',
+      description='(Forked) Trimmed mustache logic-less templates',
       author='Linh-Nam Vu',
       author_email='github@l-vu.com',
-      url='https://github.com/hyperturtle/Stache',
-      packages=['Stache'],
-      package_dir={'Stache': '.'},
+      maintainer = "Chris Clark",
+      url='https://github.com/clach04/stache',
+      packages=['stache'],
+      package_dir={'stache': '.'},
      )
